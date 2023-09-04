68895f2664c1da2c109382b0c5dba5ea2b52bd61
Jashmir Kharwar <kjashmir@gmail.com>
2023-09-04 14:36:38 -0400

    Calculate all combinations
    Calculate all combinations of an amount using 10, 50 & 100 EUR cartridges

---
+++ RIA_Question1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] denominations = { 10, 50, 100 };
+            int[] amountsToPay = { 30, 50, 60, 80, 140, 230, 370, 610, 980 };
+
+            foreach (int amount in amountsToPay)
+            {
+                Console.WriteLine($"For {amount} EUR:");
+                Console.WriteLine();
+                CalculateCombinations(denominations, amount, new List<int>(), 0);
+                Console.WriteLine();
+            }
+        }
+
+        static void CalculateCombinations(int[] denominations, int remainingAmount, List<int> currentCombination, int index)
+        {
+            if (remainingAmount == 0)
+            {
+                PrintCombination(currentCombination);
+                return;
+            }
+
+            if (remainingAmount < 0 || index >= denominations.Length)
+                return;
+
+            // Include the current denomination in the combination
+            currentCombination.Add(denominations[index]);
+            CalculateCombinations(denominations, remainingAmount - denominations[index], currentCombination, index);
+
+            // Exclude the current denomination from the combination
+            currentCombination.RemoveAt(currentCombination.Count - 1);
+            CalculateCombinations(denominations, remainingAmount, currentCombination, index + 1);
+        }
+
+        static void PrintCombination(List<int> combination)
+        {
+            if (combination.Count > 0)
+            {
+                Console.Write("*  ");
+                for (int i = 0; i < combination.Count; i++)
+                {
+                    int count = 1;
+                    while (i < combination.Count - 1 && combination[i] == combination[i + 1])
+                    {
+                        count++;
+                        i++;
+                    }
+                    Console.Write($"{count} x {combination[i]} EUR");
+                    if (i < combination.Count - 1)
+                    {
+                        Console.Write(" + ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ RIA_Question1/README.md
@@ -0,0 +1,8 @@
+# All combinations for an amount using 10, 50, 100 EUR cartridges.
+
+- This program displays all the combinations for an amount by just using 10, 50 & 100 EUR cartridges.
+- This program has 2 methods:
+  - CalculateCombinations - Which calculates all the possible combinations.
+  - PrintCombination(List(int) combination) - Which prints all the combinations for a specific amount.
+
+![Alt text](image.png)  
